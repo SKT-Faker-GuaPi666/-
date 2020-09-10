6f4117e600ad1c4b80c86cc59a0df12299a66799
森木鱼 <68533677+SKT-Faker-GuaPi666@users.noreply.github.com>
2020-09-10 23:12:38 +0800

    2020.9.10             C++学习

---
+++ point.cpp
@@ -0,0 +1,26 @@
+#include "point.h"
+//创建点类
+
+//设置x
+void Point::setX(int x)
+{
+m_X = x;
+}
+//读取x
+int Point::getX()
+{
+return m_X;
+}
+//设置y
+void Point::setY(int y)
+{
+m_Y = y;
+}
+//读取y
+int Point::getY()
+{
+return m_Y;
+}
+
+
+
